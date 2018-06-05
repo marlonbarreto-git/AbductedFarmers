cc99a8b04de67e2d900282037ebb94a22373dc8a
andreaasenav <36159489+andreaasenav@users.noreply.github.com>
2018-06-04 21:45:36 -0500

    Update Character.java

--- AbductedFarmers/src/edu/unal/modelo/Character.java
+++ AbductedFarmers/src/edu/unal/modelo/Character.java
@@ -76,5 +76,12 @@ public abstract class Character {
         return false;
     }
     
-  
+    public void getImageDimensions() {
+        width = skin.getWidth(null);
+        height = skin.getHeight(null);
+    }
+    
+     public Rectangle getBounds() {
+         return new Rectangle(posX, posY, width, height);
+     }
 }
